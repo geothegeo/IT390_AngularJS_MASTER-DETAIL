9f2a3e1e5e9a1967f222a22435bd26489a06b467
George Tang <georgethewebmaster@gmail.com>
2020-09-29 23:46:11 -0500

    Finished list displaying Avengers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'The Avengers and Their Abilities';
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+
+
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Thor', ability: 'Lightning Strike' },
+    { id: 2, name: 'Iron Man', ability: 'Hand Laser' },
+    { id: 3, name: 'Hulk', ability: 'Rage Smash' },
+    { id: 4, name: 'Captain America', ability: 'Shield Toss' },
+    { id: 5, name: 'Hawkeye', ability: 'Arrow Pierce' },
+    { id: 6, name: 'Black Widow', ability: 'Stealth Recon' }
+];
